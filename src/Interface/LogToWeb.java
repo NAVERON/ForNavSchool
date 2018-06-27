9d2068ec1b69a4ff1943d73f5850a58337d41bcd
NAVERON <2046783134@qq.com>
2018-06-27 17:40:12 +0800

    delete old, change need review code

--- src/Interface/LogToWeb.java
+++ src/Interface/LogToWeb.java
@@ -5,15 +5,10 @@
  */
 package Interface;
 
-import Logic.LoginController;
-import java.io.IOException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import Logic.SearchController;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 /**
@@ -23,12 +18,11 @@ import javafx.stage.Stage;
 public class LogToWeb extends Application {
 
     public static String PROJECT_NAME = "武汉理工大学航运学院信息搜索平台";
-    
     @Override
     public void start(Stage primaryStage) throws Exception {
         
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("./Login.fxml"));
-        LoginController logincontroller = new LoginController(primaryStage);
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("./Search.fxml"));
+        SearchController logincontroller = new SearchController(primaryStage);
         loader.setController(logincontroller);
         
         Scene scene = new Scene(loader.load());
--- src/Logic/SearchController.java
+++ src/Logic/SearchController.java
@@ -5,19 +5,10 @@
  */
 package Logic;
 
-import java.io.IOException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.stage.Stage;
 
-/**
- *
- * @author ERON
- */
 public class SearchController {
     @FXML
     Button logout;
@@ -26,17 +17,5 @@ public class SearchController {
     public SearchController(Stage primaryStage){
         this.primaryStage = primaryStage;
     }
-    
-    @FXML
-    public void changeToLogin() {
-        try {
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("../Interface/Login.fxml"));
-            LoginController logincontroller = new LoginController(this.primaryStage);
-            loader.setController(logincontroller);
-            
-            this.primaryStage.setScene(new Scene( loader.load() ));
-        } catch (IOException ex) {
-            Logger.getLogger(LoginController.class.getName()).log(Level.SEVERE, null, ex);
-        }
-    }
+
 }
