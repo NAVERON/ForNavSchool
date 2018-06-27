839a79ca002ad9028a80cf1f619d4e333d173342
NAVERON <2478327261@qq.com>
2018-06-27 22:33:09 +0800

    so hard......

--- src/Interface/SearchController.java
+++ src/Interface/SearchController.java
@@ -5,17 +5,22 @@
  */
 package Interface;
 
+import java.util.Date;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
-import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
 import javafx.scene.control.ProgressBar;
-import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.web.WebView;
 import javafx.stage.Stage;
 
 public class SearchController {
 
     private Stage primaryStage = null;
+    private Date from, to;
+    private String keywords;
 
     @FXML
     private Button search_btn;
@@ -23,9 +28,22 @@ public class SearchController {
     private ProgressBar search_progress;
     @FXML
     private HBox mannual_hbox;
+    @FXML
+    private DatePicker from_date;
+    @FXML
+    private DatePicker to_date;
+    @FXML
+    private TextField keywords_textfield;
+    @FXML
+    private VBox titles_boxs;   //左边添加链接
+    @FXML
+    private WebView content_webview;  //右边显示的超链接网页界面
 
     public SearchController(Stage primaryStage) {
         this.primaryStage = primaryStage;
     }
-
+    
+    public void search(){
+        
+    }
 }
