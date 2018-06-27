6673c782c4fb1058e473b312d409c39c7fbff82f
NAVERON <2046783134@qq.com>
2018-06-27 17:09:45 +0800

    ui

--- src/Logic/LoginController.java
+++ src/Logic/LoginController.java
@@ -12,6 +12,10 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Hyperlink;
+import javafx.scene.control.TextField;
+import javafx.scene.web.WebEngine;
+import javafx.scene.web.WebView;
 import javafx.stage.Stage;
 
 /**
@@ -19,18 +23,17 @@ import javafx.stage.Stage;
  * @author ERON
  */
 public class LoginController {
-
-    @FXML
-    Button login;
+    
+    @FXML TextField student_id_textfield; TextField student_pw_textfield; Hyperlink log_visitor; Button log_honor;
     
     
     private Stage primaryStage = null;
     public LoginController(Stage primaryStage) {
         this.primaryStage = primaryStage;
     }
-
+    
     @FXML
-    public void changeToSearch() {
+    public void changeToSearch() {  //这里需要根据是否登陆来判断载入哪个页面
         try {
             FXMLLoader loader = new FXMLLoader(getClass().getResource("../Interface/Search.fxml"));
             SearchController searchcontroller = new SearchController(this.primaryStage);
@@ -40,5 +43,19 @@ public class LoginController {
         } catch (IOException ex) {
             Logger.getLogger(LoginController.class.getName()).log(Level.SEVERE, null, ex);
         }
+    }
+    
+    public static String WEB_PAGE = "http://zhlgd.whut.edu.cn/tpass/login?service=http%3A%2F%2Fzhlgd.whut.edu.cn%2Ftp_up%2F";
+    @FXML
+    public void changeToBrowser(){
+        this.primaryStage.close();
+        
+        WebView webview = new WebView();
+        WebEngine webengine = webview.getEngine();
+        webengine.load(LoginController.WEB_PAGE);
+
+        Stage webAccess = new Stage();
+        Scene scene = new Scene(webview);
+        webAccess.show();
     }
 }
